7bb0ef3a9b9a99a8e4bac31a0f2a85bc7f758638
Haiqal-Pega <asphic99@gmail.com>
2022-09-15 12:05:34 +0800

    syntax php

--- w3syntax.php
+++ w3syntax.php
@@ -4,9 +4,11 @@
 
 <?php
 $color = "red";
-echo "My car is " . $color . "<br>";
-echo "My house is " . $COLOR . "<br>";
-echo "My boat is " . $coLOR . "<br>";
+echo "My car is " . $color . "<br>"; //will output red
+echo "My house is " . $COLOR . "<br>"; //will not output red
+echo "My boat is " . $coLOR . "<br>"; // will not output red
+
+//variable are case sensitive
 ?>
 
 </body>
