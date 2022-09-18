6e3f5e1d5bab5313b105065d78535b34aed76695
Haiqal-Pega <asphic99@gmail.com>
2022-09-18 15:31:09 +0800

    php syntax

---
+++ w3syntax.php
@@ -0,0 +1,19 @@
+<?php
+    ECHO "Hello World!<br>";
+    echo "Hello World!<br>";
+    EcHo "Hello World!<br>";
+    
+    //case sensitive on variables
+    $color = "red";
+    echo "My car is " . $color . "<br>";
+    echo "My house is " . $COLOR . "<br>";
+    echo "My boat is " . $coLOR . "<br>";
+
+    /* multiple
+    line
+    comment */
+
+    //use comment to leave out parts of code
+    $x = 5 /* + 15 */ + 5;
+    echo $x;
+?>
