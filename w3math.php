94f07c0a72074b98e33ae483e0cc1a67436336a6
Haiqal-Pega <asphic99@gmail.com>
2022-09-18 21:15:19 +0800

    php math

--- w3math.php
+++ w3math.php
@@ -12,6 +12,13 @@ echo"<br>PHP abs() Function<br>";//return absolute value
 echo("absolute of -6.7: ".abs(-6.7)."<br>");  // returns 6.7
 
 echo"<br>PHP sqrt() Function<br>"; //return square root of a number
-echo("sqrt(64): ".sqrt(64));  // returns 8
+echo("sqrt(64): ".sqrt(64)."<br>");  // returns 8
 
+echo"<br>PHP round() Function<br>"; //round off float to nearest int
+echo("round(0.60): ".round(0.60)."<br>");  // returns 1
+echo("round(0.49): ".round(0.49)."<br>");  // returns 0
+
+echo"<br>PHP Random Number Function (rand())<br>"; //generate random number
+echo("No range random: ".rand()."<br>"); //randomly generate
+echo("Ranged random: ".rand(10, 100)."<br>"); //generate random between two number
 ?>
