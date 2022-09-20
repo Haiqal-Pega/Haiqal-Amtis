536660de7ff5a8b5c234b7bd518f7b1dc925c1f5
Haiqal_Pega <asphic99@gmail.com>
2022-09-20 17:59:50 +0800

    login eCommerce

--- eCommerce/login.php
+++ eCommerce/login.php
@@ -1,7 +1,35 @@
-<!DOCTYPE html>
-<html>
-    <body>
-    Welcome <?php echo $_POST["name"]; ?><br>
-    Your email address is: <?php echo $_POST["pw"]; ?>
-    </body>
-</html>
+<?php
+    $name = $_POST["name"];
+    $pw = $_POST["pw"];
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "sys";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    } else {
+     echo"Connected<br>";
+    }
+
+
+    $sql = "SELECT user_name, user_pw FROM user"; 
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            if($row["user_name"]== $name && $row["user_pw"]== $pw)
+            echo "Welcome ". $name."<br>";
+            else{
+                $conn->close();
+                header("Location: index.html");
+            }
+        }
+      } else {
+        echo "0 results";
+      }
+?>
+
