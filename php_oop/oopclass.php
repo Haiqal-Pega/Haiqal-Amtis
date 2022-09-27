936c93c0f1e54410b47d82f3ebe88a7d41a8fa0b
Haiqal_Pega <asphic99@gmail.com>
2022-09-27 10:50:03 +0800

    oop

--- php_oop/oopclass.php
+++ php_oop/oopclass.php
@@ -24,5 +24,5 @@ $apple->set_name('Apple');
 $apple->set_color('Red');
 echo "Name : " . $apple->get_name();
 echo "<br>";
-echo "Color: " . $apple->get_color();
+echo "Color : " . $apple->get_color();
 ?>
