3aa2be5a5dbebd5994df322b65dbec26518dd9e8
Haiqal_Pega <asphic99@gmail.com>
2022-09-30 10:35:56 +0800

    TOH solved

--- ProblemSolving/TOH.php
+++ ProblemSolving/TOH.php
@@ -3,8 +3,8 @@
     $a= array();
     $b= array();
     $c= array();
-    $n=2;
-    $counter=1;
+    $n=15;
+    $counter=0;
 
     for ($x=$n; $x>0; --$x){
         array_push($a,$x);
@@ -15,12 +15,17 @@
     echo "A: [".implode(", ", $a)."] ";
     echo "B: [".implode(", ", $b)."] ";
     echo "C: [".implode(", ", $c)."] <br><br>";
-    TOHe(pow(2,$n),$counter);
-    //TOHe($n);
-    function TOHe($n,$counter){
-        global $a, $b, $c;
+    if($n%2==0){
+        TOHe();
+    }else{
+        TOHo();
+    }
+
+
+    function TOHe(){
+        global $a, $b, $c, $n, $counter;
         
-        if(empty($b) || end($a)<end($b)){
+        if(empty($b) || end($a)<end($b) && !empty($a)){
             $first = array_pop($a);
             array_push($b,$first);
         }
@@ -28,11 +33,11 @@
             $first = array_pop($b);
             array_push($a, $first);
         }
-        echo $counter;
-        echo "<br>A: [".implode(", ", $a)."] ";
+        
+        echo "A: [".implode(", ", $a)."] ";
         echo "B: [".implode(", ", $b)."] ";
         echo "C: [".implode(", ", $c)."] ";
-
+        echo "Moves #".++$counter."<br>";
 
         //#####################
         if(empty($c) ||end($a)<end($c)){
@@ -43,10 +48,11 @@
             $sec = array_pop($c);
             array_push($a, $sec);
         }
-        echo "<br>A: [".implode(", ", $a)."] ";
+        
+        echo "A: [".implode(", ", $a)."] ";
         echo "B: [".implode(", ", $b)."] ";
         echo "C: [".implode(", ", $c)."] ";
-
+        echo "Moves #".++$counter."<br>";
         
         //######################
         if(empty($b) || end($b)<end($c)){
@@ -57,14 +63,74 @@
             $tr = array_pop($c);
             array_push($b, $tr);
         }
-        echo "<br>A: [".implode(", ", $a)."] ";
+        
+        echo "A: [".implode(", ", $a)."] ";
         echo "B: [".implode(", ", $b)."] ";
         echo "C: [".implode(", ", $c)."] ";
-        echo "<br><br>";
+        echo "Moves #".++$counter."<br>";
 
-        if($n<0){
+        if($counter >= (pow(2,$n)-1)){
             return 0;
         }
-        TOHe($n-1,$counter+1);
+        TOHe();
+    }
+
+    function TOHo(){
+        global $a, $b, $c, $n, $counter;
+        
+        if(empty($c) || end($a)<end($c) && !empty($a)){
+            $first = array_pop($a);
+            array_push($c,$first);
+        }
+        elseif(empty($a) || end($a)>end($c)){
+            $first = array_pop($c);
+            array_push($a, $first);
+        }
+        
+        echo "A: [".implode(", ", $a)."] ";
+        echo "B: [".implode(", ", $b)."] ";
+        echo "C: [".implode(", ", $c)."] ";
+        echo "Moves #".++$counter."<br><br>";
+        if($counter >= (pow(2,$n)-1)){
+            return 0;
+        }
+
+        //#####################
+        if(empty($b) ||end($a)<end($b) && !empty($a)){
+            $sec = array_pop($a);   
+            array_push($b,$sec);
+        }
+        elseif(empty($a) || end($a)>end($b)){
+            $sec = array_pop($b);
+            array_push($a, $sec);
+        }
+        
+        echo "A: [".implode(", ", $a)."] ";
+        echo "B: [".implode(", ", $b)."] ";
+        echo "C: [".implode(", ", $c)."] ";
+        echo "Moves #".++$counter."<br><br>";
+        if($counter >= (pow(2,$n)-1)){
+            return 0;
+        }
+        
+        //######################
+        if(empty($b) || end($b)<end($c) && !empty($c)){
+            $tr = array_pop($b);
+            array_push($c, $tr);
+        }
+        elseif(empty($c) || end($b)>end($c)){
+            $tr = array_pop($c);
+            array_push($b, $tr);
+        }
+        
+        echo "A: [".implode(", ", $a)."] ";
+        echo "B: [".implode(", ", $b)."] ";
+        echo "C: [".implode(", ", $c)."] ";
+        echo "Moves #".++$counter."<br><br> ";
+
+        if($counter >= (pow(2,$n)-1)){
+            return 0;
+        }
+        TOHo();
     }
 ?>
