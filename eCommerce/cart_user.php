3f96225db8011c85b45eaa004ae7f7a12d83d966
Haiqal_Pega <asphic99@gmail.com>
2022-10-03 17:53:21 +0800

    cart

--- eCommerce/cart_user.php
+++ eCommerce/cart_user.php
@@ -1,5 +1,9 @@
 <?php
     session_start();
+    if(!isset($_SESSION['status'])){
+        echo "<script>alert('Please sign back in')</script>";
+        header("location:index.php");
+    }
     $user_id = $_SESSION['s_id'];
     $servername = "localhost";
     $username = "root";
@@ -7,6 +11,10 @@
     $dbname = "sys";
 
     $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,7 +27,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.1/dist/js/bootstrap.bundle.min.js"></script>
     </head>
-    <body>
+<body>
     <div class="container-fluid py-5  bg-primary text-white text-center">
         <h1>PC MasteRace.COM</h1>
             <p>Buy All You Need To Build Your Perfect PC</p> 
@@ -43,56 +51,55 @@
                 <a class="nav-link active" href="cart_user.php">Cart</a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link" href="login.html">Logout</a>
+                <a class="nav-link" href="index.php">Logout</a>
                 </li>
             </ul>
-            <form class="d-flex">
-                <input class="form-control me-2" type="text" placeholder="Search">
-                <button class="btn btn-primary" type="button">Search</button>
-            </form>
         </div>
         </div>
         </div>
         </div>      
         </div>
     </nav>
-    <?php
-        $sql = "SELECT * FROM `product` JOIN `cart` ON `product.p_id=cart.p_id` JOIN `user` ON `user.u_id=cart.u_id` "; 
-        $result = mysqli_query($conn, $sql); // First parameter is just return of "mysqli_connect()" function
+    <div class="container col-9  p-3 ">
+        <?php
+            $sql = "SELECT * FROM cart JOIN product ON cart.p_id=product.p_id WHERE cart.u_id=$user_id";
 
-        if($result->num_rows > 0) {
-            // output data of each row
-            echo '<table class="table table-hover my-3">
-            <tr>
-            <th>ID</th>
-            <th>Product Name</th>
-            <th>Price (RM)</th>
-            <th class="w-25">Descriptions</th>
-            <th>Images</th>
-            <th class="w-25">Delete/Update</th>
-            </tr>';
-            while($row = mysqli_fetch_assoc($result))
-            {
-                $pic= $row["p_image"];
-                echo '<tr>
-                <td>'.$row["p_id"].'</td>
-                <td>'.$row["p_name"].'</td>
-                <td>'.$row["p_price"].'</td>
-                <td>'.$row["p_details"].'</td>
-                <td> <img src="../eComProd/'.$pic.'" style="width:100px"></td>
-                <td> '
-                ?>
-                <form method="post" action="updateprod.php">
-                    <input class="btn btn-info  " type="submit" name="action" value="Update"/>
-                    <input class="btn btn-danger" type="submit" name="action" value="Delete"/>
-                    <input type="hidden" name="id" value="<?php echo $row['p_id']; ?>"/>
-                </form>    
-                <?php 
-                '</td></tr>';
-                
+            $result = mysqli_query($conn, $sql);
+
+                if ($result->num_rows > 0) { 
+                // output data of each row
+                echo '<table class="table table-hover my-3 text-center">
+                <tr>
+                <th>ID</th>
+                <th>Product Name</th>
+                <th>Price (RM)</th>
+                <th class="w-25">Descriptions</th>
+                <th>Images</th>
+                <th class="w-25">Action</th>
+                </tr>';
+                while($row = mysqli_fetch_assoc($result))
+                {
+                    $pic= $row["p_image"];
+                    echo '<tr>
+                    <td>'.$row["p_id"].'</td>
+                    <td>'.$row["p_name"].'</td>
+                    <td>'.$row["p_price"].'</td>
+                    <td>'.$row["p_details"].'</td>
+                    <td> <img src="../eComProd/'.$pic.'" style="width:100px"></td>
+                    <td> '
+                    ?>
+                    <form method="post" action="actioncart.php">
+                        <input class="btn btn-danger  " type="submit" name="action" value="Remove"/>
+                        <input type="hidden" name="id" value="<?php echo $row['c_id']; ?>"/>
+                    </form>    
+                    <?php 
+                    '</td></tr>';
+                    
+                }
+                echo '</table>';
             }
-            echo '</table>';
-        }
-    ?>
+        ?>
+    </div>
+    
 </body>
 </html>
